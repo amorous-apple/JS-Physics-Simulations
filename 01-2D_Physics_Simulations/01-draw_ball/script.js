1e22918ea91d27786c4735763fa3359adfe0312e
CongLong <thdragonmaster@gmail.com>
2024-09-14 21:45:21 +0200

    Drew a ball.

---
+++ 01-2D_Physics_Simulations/01-draw_ball/script.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function drawBall(x, y, r) {
+
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2 * Math.PI);
+	ctx.strokeStyle = "black";
+	ctx.stroke();
+	ctx.fillStyle = "red";
+	ctx.fill();
+
+}
+
+drawBall(200, 100, 20);
+
+
+ctx.beginPath();
+ctx.arc(400, 400, 30, 0, 2 * Math.PI);
+ctx.strokeStyle = "black";
+ctx.stroke();
+ctx.fillStyle = "green";
+ctx.fill();
