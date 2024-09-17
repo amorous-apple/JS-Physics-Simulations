f9a49f73a6b6c5efdaa0d6dc405fc2e7efab93a4
CongLong <thdragonmaster@gmail.com>
2024-09-17 21:17:29 +0200

    Ball acceleration.

--- 01-2D_Physics_Simulations/04-ball_acceleration/script.js
+++ 01-2D_Physics_Simulations/04-ball_acceleration/script.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext('2d');
 const BALLS = [];
 
 let LEFT, RIGHT, UP, DOWN;
+let friction = 0.01;
 
 
 class Ball {
@@ -11,6 +12,11 @@ class Ball {
 		this.x = x;
 		this.y = y;
 		this.r = r;
+		this.vel_x = 0;
+		this.vel_y = 0;
+		this.acc_x = 0;
+		this.acc_y = 0;
+		this.acc = .1;
 		this.player = false;
 		BALLS.push(this);
 	}
@@ -21,6 +27,19 @@ class Ball {
 		ctx.stroke();
 		ctx.fillStyle = "red";
 		ctx.fill();
+	}
+	display() {
+		ctx.beginPath();
+		ctx.moveTo(this.x, this.y);
+		ctx.lineTo(this.x + this.acc_x * 100, this.y + this.acc_y * 100);
+		ctx.strokeStyle = "green";
+		ctx.stroke();
+
+		ctx.beginPath();
+		ctx.moveTo(this.x, this.y);
+		ctx.lineTo(this.x + this.vel_x * 10, this.y + this.vel_y * 10);
+		ctx.strokeStyle = "white";
+		ctx.stroke();
 	}
 }
 
@@ -39,10 +58,21 @@ function keyControl(b) {
 		if (e.code === "ArrowDown") { DOWN = false; }
 	});
 
-	if (LEFT) { b.x--; }
-	if (UP) { b.y--; }
-	if (RIGHT) { b.x++; }
-	if (DOWN) { b.y++; }
+	if (LEFT) { b.acc_x = -b.acc; }
+	if (UP) { b.acc_y = -b.acc; }
+	if (RIGHT) { b.acc_x = b.acc; }
+	if (DOWN) { b.acc_y = b.acc; }
+	if (!UP && !DOWN) { b.acc_y = 0; }
+	if (!RIGHT && !LEFT) { b.acc_x = 0; }
+
+	b.vel_x += b.acc_x;
+	b.vel_y += b.acc_y;
+
+	b.vel_x *= 1 - friction;
+	b.vel_y *= 1 - friction;
+
+	b.x += b.vel_x;
+	b.y += b.vel_y;
 }
 
 function mainLoop() {
@@ -52,15 +82,16 @@ function mainLoop() {
 		if (b.player) {
 			keyControl(b);
 		}
+		b.display();
 	})
 	requestAnimationFrame(mainLoop);
 }
 
 let Ball1 = new Ball(200, 200, 30);
-let Ball2 = new Ball(300, 300, 20);
+// let Ball2 = new Ball(300, 300, 20);
 
 Ball1.player = true;
-Ball2.player = true;
+// Ball2.player = false;
 
 
 requestAnimationFrame(mainLoop);
